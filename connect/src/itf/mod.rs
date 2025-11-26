21361e3a89af57356142fde2fdb9860e28e04d3c
Erick Pintor <erickpintor@gmail.com>
2025-11-26 15:45:15 -0300

    Export only necessary bits from itf crate

--- connect/src/itf/mod.rs
+++ connect/src/itf/mod.rs
@@ -1,5 +1,9 @@
-pub(crate) mod option;
-pub(crate) mod reader;
-pub(crate) mod display;
+mod reader;
 
-pub use itf::*;
+pub(crate) mod display;
+pub(crate) mod option;
+pub(crate) use itf::Trace;
+
+#[doc(inline)]
+pub use itf::{Value, de, value};
+pub use reader::ValueReader;
