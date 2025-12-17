50a4169f28249e83426605b4471c68f8e94991ab
Erick Pintor <erickpintor@gmail.com>
2025-12-17 19:07:38 -0300

    Split divergent tests into nightly and stable
    It looks like there will be a small change coming to the stable release
    at some point regarding how errors are reported by rust macro expansion.
    Unfortunately, need to make sure the crate tests passes both stable and
    nightly versions of Rust, so I decided to split the divergent test into
    two versions.

--- connect/tests/macros.rs
+++ connect/tests/macros.rs
@@ -4,3 +4,17 @@ fn test_macros() {
     t.pass("tests/macros/*/pass/*.rs");
     t.compile_fail("tests/macros/*/fail/*.rs");
 }
+
+#[test]
+#[cfg(not(nightly))]
+fn test_macros_stable() {
+    let t = trybuild::TestCases::new();
+    t.compile_fail("tests/macros/*/fail/stable/*.rs");
+}
+
+#[test]
+#[cfg(nightly)]
+fn test_macros_nightly() {
+    let t = trybuild::TestCases::new();
+    t.compile_fail("tests/macros/*/fail/nightly/*.rs");
+}
